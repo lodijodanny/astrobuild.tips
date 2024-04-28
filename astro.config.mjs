52b91849f9b0f387b82868c8cf353eca2aaca687
lodijodanny <dannyws@gmail.com>
2024-04-28 16:52:35 -0500

    Creación de Header Top con componente de Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import vue from "@astrojs/vue";
-import svelte from "@astrojs/svelte";
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  integrations: [tailwind(), mdx(), react(), vue(), svelte()],
+  build: {
+    // Configura la optimización de HTML
+    optimize: {
+      html: {
+        minify: false, // Desactiva la minificación de HTML
+      },
+    },
+  },
 });
